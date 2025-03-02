630993b1f76d0882235b8da8b335442c239e9f46
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2025-03-02 22:51:13 +0900

    feat: devops-api-gateway 초본 작성

---
+++ src/main/java/org/example/devopsapigateway/DevopsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.example.devopsapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/devopsapigateway/common/config/WebfluxConfig.java
@@ -0,0 +1,19 @@
+package org.example.devopsapigateway.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class WebfluxConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .maxAge(3600);
+    }
+}
